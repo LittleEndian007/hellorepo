4460c92f75235a520d5787a0c5c90c4eee3766bc
LittleEndian007 <LittleEndian007@gmail.com>
2019-01-07 07:46:30 +0000

    Made chanages to the Main.cpp file

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,6 +4,7 @@ int main()
 {
 	std::cout << "Hello Repo!" << std::endl;
 	std::cout << "This will be a test project to upload to GitHub..." << std::endl;
-
+	std::cout << "These are the changes that are commited" << std::endl;
+	
 	return 0;
 }
